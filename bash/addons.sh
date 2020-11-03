94c239355940588a64cbc9aeed55de595329cd57
Trey Simmons <yert511@gmail.com>
2020-11-03 09:40:59 -0600

    Create addons script file

---
+++ bash/addons.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# -a: Add listed folders
+# -b: Backup listed folders
+# -h: Display help/usage message
+# -r: Remove listed folders
+# -s: Set source location
+# -u: Update listed folders
