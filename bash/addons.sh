17f23e8d27099e1eb48927509ea0062ab28df539
Trey Simmons <yert511@gmail.com>
2020-11-03 13:01:34 -0600

    Added mode selection functionality

--- bash/addons.sh
+++ bash/addons.sh
@@ -18,15 +18,20 @@ displayHelpMessage () {
   printf "%s\n" "Syntax: addons.sh {-a|-b|-B|-h|-r|-s|-u|-U} [FILE_PATHS...]"
 }
 
+MODES=()
+
 while getopts ":abB:hrs:uU:" opt; do
   case "$opt" in
     a)
+      MODES+=("a")
       # copy from source to addon_dir
       ;;
     b)
+      MODES+=("b")
       # copy from addon_dir to addon/file_name.orig
       ;;
     B)
+      MODES+=("B")
       BACKUP_DEST="$OPTARG"
       # copy from to dest/file_name.orig
       ;;
@@ -35,6 +40,7 @@ while getopts ":abB:hrs:uU:" opt; do
       exit 0
       ;;
     r)
+      MODES+=("r")
       # generate warning message/wait for response
       # rm from addon_dir
       ;;
@@ -42,10 +48,12 @@ while getopts ":abB:hrs:uU:" opt; do
       SOURCE="$OPTARG"
       ;;
     u)
+      MODES+=("u")
       # copy matching addons from addon_dir to addon/file_name.orig
       # copy matching addons from source to addon folder
       ;;
     U)
+      MODES+=("U")
       BACKUP_DEST="$OPTARG"
       # copy matching addons from addon_dir to dest/file_name.orig
       # copy matching addons from source to addon folder
@@ -65,4 +73,11 @@ shift $(($OPTIND - 1))
 if [ "$#" -eq 0 ]; then
   printf "%s\n" "Missing argument: Use the -h flag to learn about required arguments." >&2
   exit 1
+fi
+
+if [ "${#MODES[@]}" -gt 1 ]; then
+  printf "%s\n" "Mode conflict: Only one operation mode can be chosen. Use the -h flag to learn more about mode options." >&2
+  exit 1
+else
+  MODE="${MODES[0]}"
 fi
